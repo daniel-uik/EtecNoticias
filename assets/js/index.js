384e4efbaafc1f1117a1b794a48953273bfa0495
daniel-uik <daniel.alvesdosantos0@gmail.com>
2023-08-18 15:27:17 -0300

    mds

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,14 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
   const body = document.body;
   const cabecalhoNav = document.querySelector('.cabecalho');
   const conteudoMain = document.querySelector('.conteudo');
-  const TeamCont = document.querySelector('.team-container');
+  const conteudoMain1 = document.querySelector('.conteudo1');
   const texts = document.querySelectorAll('h1, h2, h3, p, a');
   const rodapeFooter = document.querySelector('#footer');
+  const title = document.querySelector('.article-title');
 
   modoEscuroToggle.addEventListener("click", function() {
     body.classList.toggle("modo-escuro2");
     cabecalhoNav.classList.toggle('modo-escuro');
     conteudoMain.classList.toggle('modo-escuro2');
+    conteudoMain1.classList.toggle('modo-escuro2');
     rodapeFooter.classList.toggle('modo-escuro');
 
     texts.forEach(text => text.classList.toggle('modo-claro'));
